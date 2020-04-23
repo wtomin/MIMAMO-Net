6f656b684fc1360114c33f546489be2cbcbd5459
Didan Deng <33117903+wtomin@users.noreply.github.com>
2020-04-23 16:01:35 +0800

    Add files via upload

---
+++ api/models/download_models.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Lb-Z9iJR7uCtIEa5CEtijyqevesO2tpq' -O model_weights.pth.tar
+
+echo "MIMAMO Net model weights have been downloaded."
